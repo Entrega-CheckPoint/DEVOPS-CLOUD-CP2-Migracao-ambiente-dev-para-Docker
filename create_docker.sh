006570dca3f9c17d043afa3592e13f20a73b6e0e
FrancescoDiBenedetto <francescomdibe@gmail.com>
2025-05-10 20:50:32 -0300

    atualizar a criação do mysql

--- create_docker.sh
+++ create_docker.sh
@@ -1,10 +1,18 @@
+# docker run -d --name mysql-rm557313 --network rm557313-net -e MYSQL_ROOT_PASSWORD=senha123 -e MYSQL_DATABASE=mottuDB -p 3306:3306 mysql/mysql-server:latest
+
 cd deploy
 
 docker network create rm557313-net
 
 docker build -t mottu-api .
 
-docker run -d --name mysql-rm557313 --network rm557313-net -e MYSQL_ROOT_PASSWORD=senha123 -e MYSQL_DATABASE=mottuDB -p 3306:3306 mysql:8.0
+docker run -d \
+    --name mysql-rm557313 \
+    --network rm557313-net \
+    -e MYSQL_ROOT_PASSWORD=senha123 \
+    -e MYSQL_DATABASE=mottuDB \
+    -p 3306:3306 \
+    mysql/mysql-server:latest
 
 docker run -d --name adminer-rm557313 --network rm557313-net -p 8081:8080 adminer
 
